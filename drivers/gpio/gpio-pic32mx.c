03d46f94c394ccad4fb3ff13c9bfebd24e0ebcc7
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-02-24 18:58:56 +0100

    gpio: support for PIC32MX

---
+++ drivers/gpio/gpio-pic32mx.c
@@ -0,0 +1,48 @@
+#include "gpio-pic32mx.h"
+#include "picoRTOS.h"
+
+#include <stdint.h>
+#include "misc-pic32mx.h"
+
+struct GPIO_PIC32MX {
+    struct PIC32MX_CLR_SET_INV RESERVED0[2];
+    struct PIC32MX_CLR_SET_INV PORT;
+    struct PIC32MX_CLR_SET_INV LAT;
+};
+
+/* Function: gpio_pic32mx_init
+ * Initializes a GPIO
+ *
+ * Parameters:
+ *  ctx - The GPIO context to init
+ *  base - The GPIO port base address
+ *  pin - The GPIO port pin number
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
+int gpio_pic32mx_init(struct gpio *ctx, struct GPIO_PIC32MX *base, size_t pin)
+{
+    if (!picoRTOS_assert(pin < (size_t)GPIO_PIC32MX_PIN_COUNT)) return -EINVAL;
+
+    ctx->base = base;
+    ctx->mask = (uint32_t)(1 << pin);
+
+    return 0;
+}
+
+void gpio_write(struct gpio *ctx, bool value)
+{
+    if (value) ctx->base->LAT.SET = ctx->mask;
+    else ctx->base->LAT.CLR = ctx->mask;
+}
+
+bool gpio_read(struct gpio *ctx)
+{
+    return (ctx->base->PORT.REG & ctx->mask) != 0;
+}
+
+void gpio_toggle(struct gpio *ctx)
+{
+    ctx->base->LAT.INV = ctx->mask;
+}
---
+++ drivers/gpio/gpio-pic32mx.h
@@ -0,0 +1,25 @@
+#ifndef GPIO_PIC32MX_H
+#define GPIO_PIC32MX_H
+
+#include "gpio.h"
+#include <stdint.h>
+#include <stddef.h>
+
+struct GPIO_PIC32MX; /* PORT */
+
+#define GPIO_PIC32MX_PIN_COUNT 16
+
+struct gpio {
+    /*@temp@*/ struct GPIO_PIC32MX *base;
+    uint32_t mask;
+};
+
+int gpio_pic32mx_init(/*@out@*/ struct gpio *ctx, struct GPIO_PIC32MX *base, size_t pin);
+
+/* Runtime calls:
+ * void gpio_write(struct gpio *ctx, bool value);
+ * bool gpio_read(struct gpio *ctx);
+ * void gpio_toggle(struct gpio *ctx);
+ */
+
+#endif
