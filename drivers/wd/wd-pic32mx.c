b4f832cb10252d8aee0e3afb574bb79fb979f30b
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-02-27 21:59:11 +0100

    watchdog: support for pic32mx series basic wdt

---
+++ drivers/wd/wd-pic32mx.c
@@ -0,0 +1,72 @@
+#include "wd-pic32mx.h"
+#include "picoRTOS.h"
+
+#include <stdint.h>
+#include "misc-pic32mx.h"
+
+struct WD_PIC32MX {
+    struct PIC32MX_CLR_SET_INV WDTCON;
+};
+
+#define WDTCON_WDTCLRKEY_M  0xffffu
+#define WDTCON_WDTCLRKEY(x) (((x) & WDTCON_WDTCLRKEY_M) << 16)
+#define WDTCON_ON           (1 << 15)
+#define WDTCON_RUNDIV_M     0xfu
+#define WDTCON_RUNDIV(x)    (((x) & WDTCON_RUNDIV_M) << 8)
+#define WDTCON_WDTWINEN     (1 << 0)
+
+/* Function: wd_pic32mx_init
+ * Initializes a watchdog
+ *
+ * Parameters:
+ *  ctx - The WD context to init
+ *  base - The WD base address
+ *
+ * Returns:
+ * Always 0
+ */
+int wd_pic32mx_init(struct wd *ctx, struct WD_PIC32MX *base)
+{
+    ctx->base = base;
+    return 0;
+}
+
+/* Function: wd_pic32mx_setup
+ * Configures the watchdog
+ *
+ * Parameters:
+ *  ctx - The watchdog to init
+ *  settings - The settings to apply
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
+int wd_pic32mx_setup(struct wd *ctx, struct wd_pic32mx_settings *settings)
+{
+    if (!picoRTOS_assert(settings->timeout < WD_PIC32MX_PERIOD_COUNT)) return -EINVAL;
+
+    ctx->base->WDTCON.CLR = (uint32_t)WDTCON_RUNDIV(WDTCON_RUNDIV_M);
+    ctx->base->WDTCON.SET = (uint32_t)WDTCON_RUNDIV(settings->timeout);
+
+    if (settings->enable_windowed)
+        ctx->base->WDTCON.SET = (uint32_t)WDTCON_WDTWINEN;
+
+    return 0;
+}
+
+int wd_start(struct wd *ctx)
+{
+    ctx->base->WDTCON.SET = (uint32_t)WDTCON_ON;
+    return 0;
+}
+
+int wd_stop(struct wd *ctx)
+{
+    ctx->base->WDTCON.CLR = (uint32_t)WDTCON_ON;
+    return 0;
+}
+
+void wd_refresh(struct wd *ctx)
+{
+    ctx->base->WDTCON.SET = (uint32_t)WDTCON_WDTCLRKEY(0x5743);
+}
---
+++ drivers/wd/wd-pic32mx.h
@@ -0,0 +1,52 @@
+#ifndef WD_PIC32MX_H
+#define WD_PIC32MX_H
+
+#include "wd.h"
+#include <stdbool.h>
+
+struct WD_PIC32MX;
+
+struct wd {
+    /*@temp@*/ struct WD_PIC32MX *base;
+};
+
+typedef enum {
+    WD_PIC32MX_PERIOD_1MS       = 0,
+    WD_PIC32MX_PERIOD_2MS       = 1,
+    WD_PIC32MX_PERIOD_4MS       = 2,
+    WD_PIC32MX_PERIOD_8MS       = 3,
+    WD_PIC32MX_PERIOD_16MS      = 4,
+    WD_PIC32MX_PERIOD_32MS      = 5,
+    WD_PIC32MX_PERIOD_64MS      = 6,
+    WD_PIC32MX_PERIOD_128MS     = 7,
+    WD_PIC32MX_PERIOD_256MS     = 8,
+    WD_PIC32MX_PERIOD_512MS     = 9,
+    WD_PIC32MX_PERIOD_1024MS    = 10,
+    WD_PIC32MX_PERIOD_2048MS    = 11,
+    WD_PIC32MX_PERIOD_4096MS    = 12,
+    WD_PIC32MX_PERIOD_8192MS    = 13,
+    WD_PIC32MX_PERIOD_16384MS   = 14,
+    WD_PIC32MX_PERIOD_32768MS   = 15,
+    WD_PIC32MX_PERIOD_65535MS   = 16,
+    WD_PIC32MX_PERIOD_131072MS  = 17,
+    WD_PIC32MX_PERIOD_262144MS  = 18,
+    WD_PIC32MX_PERIOD_524288MS  = 19,
+    WD_PIC32MX_PERIOD_1048576MS = 20,
+    WD_PIC32MX_PERIOD_COUNT
+} wd_pic32mx_period_t;
+
+struct wd_pic32mx_settings {
+    wd_pic32mx_period_t timeout;
+    bool enable_windowed;
+};
+
+int wd_pic32mx_init(/*@out@*/ struct wd *ctx, struct WD_PIC32MX *base);
+int wd_pic32mx_setup(struct wd *ctx, struct wd_pic32mx_settings *settings);
+
+/* Runtime calls:
+ * int wd_start(struct wd *ctx);
+ * int wd_stop(struct wd *ctx);
+ * void wd_refresh(struct wd *ctx);
+ */
+
+#endif
