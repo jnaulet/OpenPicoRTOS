b5b0611e579cff42c93dd56b9f6918caeaa5c731
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-02 11:45:17 +0200

    port: migrated cm4f to startup

--- arch/arm/cm4f/picoRTOS_port.c
+++ arch/arm/cm4f/picoRTOS_port.c
@@ -11,44 +11,22 @@
 
 /* NVIC */
 #define NVIC_ISER         ((volatile unsigned long*)0xe000e100)
-#define NVIC_ICSR         ((volatile unsigned long*)0xe000ed04)
 #define NVIC_SHPR3        ((volatile unsigned long*)0xe000ed20)
 
 /* VTOR */
 #define VTOR ((volatile unsigned long*)0xe000ed08)
 
-/* CACHES */
-#ifdef ADDR_CMCC
-# define CMCC_CTRL ((volatile unsigned long*)(ADDR_CMCC + 0x8))
-#endif
-
 /* ASM */
-/*@external@*/ extern void arch_SYSTICK(void);
-/*@external@*/ extern void arch_PENDSV(void);
-/*@external@*/ extern void arch_enable_vfp(void);
+/*@external@*/ extern /*@temp@*/
+picoRTOS_stack_t *arch_save_first_context(picoRTOS_stack_t *sp,
+                                          picoRTOS_task_fn_t fn,
+                                          void *priv);
+
 /*@external@*/ extern void arch_start_first_task(picoRTOS_stack_t *sp);
 /*@external@*/ extern void arch_syscall(picoRTOS_syscall_t syscall, void *priv);
 /*@external@*/ extern picoRTOS_atomic_t arch_compare_and_swap(picoRTOS_atomic_t *var,
                                                               picoRTOS_atomic_t old,
                                                               picoRTOS_atomic_t val);
-
-/* see cm0+ port for more info */
-#ifdef DEVICE_MOVE_VTABLE_TO_RAM
-/* vector table */
-#define VTABLE_COUNT (16 + DEVICE_INTERRUPT_VECTOR_COUNT)
-static unsigned long VTABLE[VTABLE_COUNT] __attribute__((aligned(128)));
-
-static void move_vtable_to_ram(void)
-{
-    int n = VTABLE_COUNT;
-    unsigned long *old_VTABLE = (unsigned long*)*VTOR;
-
-    while (n-- != 0)
-        VTABLE[n] = old_VTABLE[n];
-
-    *VTOR = (unsigned long)VTABLE;
-}
-#endif
 
 /* FUNCTIONS TO IMPLEMENT */
 
@@ -57,31 +35,13 @@ void arch_init(void)
     /* disable interrupts */
     ASM("cpsid if");
 
-    /* CACHES (Cortex M Cache Controller) */
-#ifdef DEVICE_ENABLE_CMCC
-    *CMCC_CTRL = 0x1ul;
-#endif
-
-    /* VFP */
-    arch_enable_vfp();
-
-    /* INTERRUPTS */
-#ifdef DEVICE_MOVE_VTABLE_TO_RAM
-    move_vtable_to_ram();
-#else
-    unsigned long *VTABLE = (unsigned long*)*VTOR;
-#endif
-    VTABLE[14] = (unsigned long)arch_PENDSV;
-    VTABLE[15] = (unsigned long)arch_SYSTICK;
-
     /* set SYSTICK & PENDSV to min priority */
     *NVIC_SHPR3 |= 0xffff0000ul;
 
     /* SYSTICK */
-    *SYSTICK_CSR &= ~1ul;                                       /* stop systick */
-    *SYSTICK_CVR = 0ul;                                         /* reset */
-    *SYSTICK_RVR = (unsigned long)SYSTICK_RVR_VALUE;            /* set period */
-    *SYSTICK_CSR |= 0x7ul;                                      /* enable interrupt & start */
+    *SYSTICK_CSR = 0x6ul;                                   /* stop systick */
+    *SYSTICK_CVR = 0;                                       /* reset */
+    *SYSTICK_RVR = (unsigned long)SYSTICK_RVR_VALUE;        /* set period */
 }
 
 void arch_suspend(void)
@@ -99,25 +59,8 @@ void arch_resume(void)
 picoRTOS_stack_t *arch_prepare_stack(struct picoRTOS_task *task)
 {
     /* ARMs have a decrementing stack */
-    picoRTOS_stack_t *sp = task->stack + task->stack_count;
-
-    /* ARM v6 reference manual, section B1.5.6 */
-    sp -= ARCH_INITIAL_STACK_COUNT;
-
-    /* sp[49] = reserved */
-    /* sp[48] = fpscr */
-    /* sp[47-32] = s15...s0 */
-
-    sp[31] = (picoRTOS_stack_t)0x1000000;       /* xspr */
-    sp[30] = (picoRTOS_stack_t)task->fn;        /* return address */
-    sp[29] = (picoRTOS_stack_t)picoRTOS_start;  /* lr (r14) */
-    /* sp[28-25] = r12, r3, r2 and r1 and */
-    sp[24] = (picoRTOS_stack_t)task->priv;      /* r0 */
-
-    /* sp[23-16] = r11..r4 */
-    /* sp[15-0] = fpu s16...s31 */
-
-    return sp;
+    return arch_save_first_context(task->stack + task->stack_count,
+                                   task->fn, task->priv);
 }
 
 /* cppcheck-suppress constParameter */
@@ -144,18 +87,11 @@ extern struct {
     /*@temp@*/ /*@null@*/ void *priv;
 } ISR_TABLE[DEVICE_INTERRUPT_VECTOR_COUNT];
 
-/*@external@*/ extern void arch_NVIC_handler(void);
-
 void arch_register_interrupt(picoRTOS_irq_t irq, picoRTOS_isr_fn fn, void *priv)
 {
     if (!picoRTOS_assert_fatal(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT))
         return;
 
-#ifndef DEVICE_MOVE_VTABLE_TO_RAM
-    unsigned long *VTABLE = (unsigned long*)*VTOR;
-#endif
-
-    VTABLE[16 + irq] = (unsigned long)arch_NVIC_handler;
     ISR_TABLE[irq].fn = fn;
     ISR_TABLE[irq].priv = priv;
 }
