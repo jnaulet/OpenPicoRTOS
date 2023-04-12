d584dad8ebdd3138741b3229dad471c6f013735a
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-12 16:16:22 +0200

    port: migrated mips32/m51xx to standalone startup

--- arch/mips32/m51xx/picoRTOS_port.c
+++ arch/mips32/m51xx/picoRTOS_port.c
@@ -6,17 +6,17 @@
 #define INTC_IFSn   ((volatile unsigned long*)(ADDR_INTC + 0x40))
 #define INTC_IECn   ((volatile unsigned long*)(ADDR_INTC + 0xc0))
 #define INTC_IPCn   ((volatile unsigned long*)(ADDR_INTC + 0x140))
-#define INTC_OFFn   ((volatile unsigned long*)(ADDR_INTC + 0x540))
+
+/*@external@*/ extern /*@temp@*/
+picoRTOS_stack_t *arch_save_first_context(picoRTOS_stack_t *sp,
+                                          picoRTOS_task_fn_t fn,
+                                          void *priv);
 
 /*@external@*/ extern void arch_start_first_task(picoRTOS_stack_t *sp);
 /*@external@*/ extern void arch_syscall(picoRTOS_syscall_t syscall, void *priv);
 /*@external@*/ extern picoRTOS_atomic_t arch_compare_and_swap(picoRTOS_atomic_t *var,
                                                               picoRTOS_atomic_t old,
                                                               picoRTOS_atomic_t val);
-
-/* interrupts */
-/*@external@*/ extern void arch_CORE_TIMER(void);
-/*@external@*/ extern void arch_SYSCALL(void);
 
 /* stats */
 /*@external@*/ extern picoRTOS_cycles_t arch_counter(void);
@@ -28,17 +28,13 @@ void arch_init(void)
     /* disable interrupts */
     ASM("di");
 
-    /* register vector table & timer interrupt */
-    INTC_OFFn[0] = (unsigned long)arch_CORE_TIMER;
-    INTC_OFFn[1] = (unsigned long)arch_SYSCALL;
-
     /* set multi-vector mode */
-    *INTC_INTCON |= (1ul << 12);
+    *INTC_INTCON |= (1 << 12);
 
     /* enable interrupts */
-    INTC_IFSn[0] &= ~0x3;
-    INTC_IECn[0] |= 0x3;
-    INTC_IPCn[0] |= 0x505;
+    INTC_IFSn[0] &= ~0x1;
+    INTC_IECn[0] |= 0x1;
+    INTC_IPCn[0] |= 0x5;
 }
 
 void arch_suspend(void)
@@ -56,15 +52,8 @@ void arch_resume(void)
 picoRTOS_stack_t *arch_prepare_stack(struct picoRTOS_task *task)
 {
     /* MPIS32s have a decrementing stack */
-    picoRTOS_stack_t *sp = task->stack + task->stack_count;
-
-    sp -= ARCH_INITIAL_STACK_COUNT;
-
-    sp[2] = (picoRTOS_stack_t)task->priv;           /* A0 */
-    sp[25] = (picoRTOS_stack_t)picoRTOS_start;      /* RA */
-    sp[28] = (picoRTOS_stack_t)task->fn;            /* EPC */
-
-    return sp;
+    return arch_save_first_context(task->stack + task->stack_count,
+                                   task->fn, task->priv);
 }
 
 /* cppcheck-suppress constParameter */
@@ -91,16 +80,12 @@ extern struct {
     /*@temp@*/ /*@null@*/ void *priv;
 } ISR_TABLE[DEVICE_INTERRUPT_VECTOR_COUNT];
 
-/*@external@*/
-extern void arch_EIC_handler(void);
-
 void arch_register_interrupt(picoRTOS_irq_t irq, picoRTOS_isr_fn fn, void *priv)
 {
     if (!picoRTOS_assert_fatal(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT)) return;
 
     ISR_TABLE[irq].fn = fn;
     ISR_TABLE[irq].priv = priv;
-    INTC_OFFn[irq] = (unsigned long)arch_EIC_handler;
 }
 
 void arch_enable_interrupt(picoRTOS_irq_t irq)
--- arch/mips32/m51xx/picoRTOS_types.h
+++ arch/mips32/m51xx/picoRTOS_types.h
@@ -1,6 +1,9 @@
 #ifndef PICORTOS_TYPES_H
 #define PICORTOS_TYPES_H
 
+/* FIXME: debatable */
+#include "picoRTOS_device.h"
+
 typedef unsigned long picoRTOS_stack_t;
 typedef unsigned long picoRTOS_tick_t;
 typedef unsigned long picoRTOS_priority_t;
@@ -9,8 +12,24 @@ typedef unsigned long picoRTOS_atomic_t;
 typedef unsigned long picoRTOS_irq_t;
 typedef unsigned long picoRTOS_cycles_t;
 
-#define ARCH_INITIAL_STACK_COUNT 30
-#define ARCH_MIN_STACK_COUNT (ARCH_INITIAL_STACK_COUNT + 2) /* ? */
+#define ARCH_INITIAL_CPU_STACK_COUNT 29
+#define ARCH_INITIAL_FPU_STACK_COUNT 0
+#define ARCH_INITIAL_DSP_STACK_COUNT 0
+
+#ifdef MIPS32_M51XX_HAS_FPU64
+# undef ARCH_INITIAL_FPU_STACK_COUNT
+# define ARCH_INITIAL_FPU_STACK_COUNT 66
+#endif
+#ifdef MIPS32_M51XX_HAS_DSP
+# undef ARCH_INITIAL_DSP_STACK_COUNT
+# define ARCH_INITIAL_DSP_STACK_COUNT 6
+#endif
+
+#define ARCH_INITIAL_STACK_COUNT (ARCH_INITIAL_CPU_STACK_COUNT +  \
+                                  ARCH_INITIAL_FPU_STACK_COUNT +  \
+                                  ARCH_INITIAL_DSP_STACK_COUNT)
+
+#define ARCH_MIN_STACK_COUNT     (ARCH_INITIAL_STACK_COUNT + 4)
 
 /* splint doesn't like inline assembly */
 #ifdef S_SPLINT_S
