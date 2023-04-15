ba35382ca8a6cc8eb47d8aded40cc527a06369e5
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-15 17:30:34 +0200

    backports: avrxmega3 updates from v1.8.x

--- arch/avr/avrxmega3/picoRTOS_types.h
+++ arch/avr/avrxmega3/picoRTOS_types.h
@@ -10,7 +10,12 @@ typedef unsigned char picoRTOS_atomic_t;
 typedef unsigned int picoRTOS_irq_t;
 typedef unsigned int picoRTOS_cycles_t;
 
-#define ARCH_INTIAL_STACK_COUNT 35
+#ifdef __AVR_3_BYTE_PC__
+# define ARCH_INTIAL_STACK_COUNT 36
+#else
+# define ARCH_INTIAL_STACK_COUNT 35
+#endif
+
 #define ARCH_MIN_STACK_COUNT (ARCH_INTIAL_STACK_COUNT + 6)
 
 /* splint cannot check inline assembly */
