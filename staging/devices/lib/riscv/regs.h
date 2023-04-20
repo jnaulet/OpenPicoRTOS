efe3ce79a5af03b21d443df655679bb206f63314
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-21 01:19:45 +0200

    devices: macros for riscv boot (staging)

---
+++ staging/devices/lib/riscv/regs.h
@@ -0,0 +1,38 @@
+#ifndef RISCV_REGS_H
+#define RISCV_REGS_H
+
+#define zero x0
+#define ra   x1
+#define sp   x2
+#define gp   x3
+#define tp   x4
+#define t0   x5
+#define t1   x6
+#define t2   x7
+#define s0   x8
+#define fp   x8
+#define s1   x9
+#define a0   x10
+#define a1   x11
+#define a2   x12
+#define a3   x13
+#define a4   x14
+#define a5   x15
+#define a6   x16
+#define a7   x17
+#define s2   x18
+#define s3   x19
+#define s4   x20
+#define s5   x21
+#define s6   x22
+#define s7   x23
+#define s8   x24
+#define s9   x25
+#define s10  x26
+#define s11  x27
+#define t3   x28
+#define t4   x29
+#define t5   x30
+#define t6   x31
+
+#endif
