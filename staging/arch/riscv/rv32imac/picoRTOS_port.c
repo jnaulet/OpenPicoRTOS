04db043cd1db915ff7e5804b258adb87295cf3b4
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-21 13:34:41 +0200

    port: riscv rv32imac support (staging)

---
+++ staging/arch/riscv/rv32imac/picoRTOS_port.c
@@ -0,0 +1,73 @@
+#include "picoRTOS.h"
+#include "picoRTOS_port.h"
+#include "picoRTOS_device.h"
+
+/*@external@*/ extern /*@temp@*/
+picoRTOS_stack_t *arch_save_first_context(picoRTOS_stack_t *sp,
+                                          picoRTOS_task_fn_t fn,
+                                          void *priv);
+
+/*@external@*/ extern void arch_suspend(void);
+/*@external@*/ extern void arch_resume(void);
+/*@external@*/ extern void arch_start_first_task(picoRTOS_stack_t *sp);
+/*@external@*/ extern void arch_syscall(picoRTOS_syscall_t syscall, void *priv);
+/*@external@*/ extern picoRTOS_atomic_t arch_compare_and_swap(picoRTOS_atomic_t *var,
+                                                              picoRTOS_atomic_t old,
+                                                              picoRTOS_atomic_t val);
+
+/* stats */
+/*@external@*/ extern picoRTOS_cycles_t arch_counter(void);
+
+/* FUNCTIONS TO IMPLEMENT */
+
+void arch_init(void)
+{
+    /* nothing to do, so far */
+}
+
+picoRTOS_stack_t *arch_prepare_stack(struct picoRTOS_task *task)
+{
+    /* RISC-Vs have a decrementing stack */
+    return arch_save_first_context(task->stack + task->stack_count,
+                                   task->fn, task->priv);
+}
+
+/* cppcheck-suppress constParameter */
+void __attribute__((weak)) arch_idle(void *null)
+{
+    if (!picoRTOS_assert_fatal(null == NULL)) return;
+
+    for (;;)
+        ASM("wfi");
+}
+
+/* ATOMIC OPS */
+
+picoRTOS_atomic_t arch_test_and_set(picoRTOS_atomic_t *ptr)
+{
+    return arch_compare_and_swap(ptr, 0, (picoRTOS_atomic_t)1);
+}
+
+/*@external@*/
+extern struct {
+    picoRTOS_isr_fn fn;
+    /*@temp@*/ /*@null@*/ void *priv;
+} ISR_TABLE[DEVICE_INTERRUPT_VECTOR_COUNT];
+
+void arch_register_interrupt(picoRTOS_irq_t irq, picoRTOS_isr_fn fn, void *priv)
+{
+    if (!picoRTOS_assert_fatal(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT)) return;
+
+    ISR_TABLE[irq].fn = fn;
+    ISR_TABLE[irq].priv = priv;
+}
+
+void arch_enable_interrupt(picoRTOS_irq_t irq)
+{
+    if (!picoRTOS_assert_fatal(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT)) return;
+}
+
+void arch_disable_interrupt(picoRTOS_irq_t irq)
+{
+    if (!picoRTOS_assert_fatal(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT)) return;
+}
