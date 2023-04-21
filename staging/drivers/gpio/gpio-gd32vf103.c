7a1141591444433d77a9bf31e21c669d7fc8ceb3
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-21 20:35:38 +0200

    gpio: support for gd32vf103 (staging)

---
+++ staging/drivers/gpio/gpio-gd32vf103.c
@@ -0,0 +1,38 @@
+#include "gpio-gd32vf103.h"
+#include "picoRTOS.h"
+
+#include <stdint.h>
+
+struct GPIO_GD32VF103 {
+    volatile uint32_t GPIOx_CTLn[2];
+    volatile uint32_t GPIOx_ISTAT;
+    volatile uint32_t GPIOx_OCTL;
+    volatile uint32_t GPIOx_BC;
+    volatile uint32_t GPIOx_LOCK;
+};
+
+int gpio_gd32vf103_init(struct gpio *ctx, struct GPIO_GD32VF103 *base, size_t pin)
+{
+    if (!picoRTOS_assert(pin < (size_t)GPIO_GD32VF103_PIN_COUNT)) return -EINVAL;
+
+    ctx->base = base;
+    ctx->mask = (uint32_t)(1 << pin);
+
+    return 0;
+}
+
+void gpio_write(struct gpio *ctx, bool value)
+{
+    if (value) ctx->base->GPIOx_OCTL |= ctx->mask;
+    else ctx->base->GPIOx_OCTL &= ~ctx->mask;
+}
+
+bool gpio_read(struct gpio *ctx)
+{
+    return (ctx->base->GPIOx_ISTAT & ctx->mask) != 0;
+}
+
+void gpio_toggle(/*@unused@*/ struct gpio *ctx __attribute__((unused)))
+{
+    picoRTOS_break(); /* TBD */
+}
---
+++ staging/drivers/gpio/gpio-gd32vf103.h
@@ -0,0 +1,25 @@
+#ifndef GPIO_GD32VF103_H
+#define GPIO_GD32VF103_H
+
+#include "gpio.h"
+#include <stddef.h>
+#include <stdint.h>
+
+#define GPIO_GD32VF103_PIN_COUNT 16
+
+struct GPIO_GD32VF103;
+
+struct gpio {
+    /*@temp@*/ struct GPIO_GD32VF103 *base;
+    uint32_t mask;
+};
+
+int gpio_gd32vf103_init(/*@out@*/ struct gpio *ctx, struct GPIO_GD32VF103 *base, size_t pin);
+
+/* Implements:
+ * void gpio_write(struct gpio *ctx, bool value);
+ * bool gpio_read(struct gpio *ctx);
+ * void gpio_toggle(struct gpio *ctx);
+ */
+
+#endif
