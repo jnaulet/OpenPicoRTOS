d9daadbb98143679298b2501cac0f9a1b57cc5f8
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-22 17:29:55 +0200

    wd: support for freerunning gd32vf103 (staging)

---
+++ staging/drivers/wd/wd-gd32vf103_fwdgt.c
@@ -0,0 +1,70 @@
+#include "wd-gd32vf103_fwdgt.h"
+#include "picoRTOS.h"
+
+#include <stdint.h>
+
+#define CMD_DISABLE 0x5555
+#define CMD_START   0xcccc
+#define CMD_RELOAD  0xaaaa
+
+struct WD_GD32VF103_FWDGT {
+    volatile uint32_t FWDGT_CTL;
+    volatile uint32_t FWDGT_PSC;
+    volatile uint32_t FWDGT_RLD;
+    volatile uint32_t FWDGT_STAT;
+};
+
+#define FWDGT_PSC_PSC_M  0x7u
+#define FWDGT_PSC_PSC(x) ((x) & FWDGT_PSC_PSC_M)
+
+#define FWDGT_RLD_RLD_M  0xfffu
+#define FWDGT_RLD_RLD(x) ((x) & FWDGT_RLD_RLD_M)
+
+int wd_gd32vf103_fwdgt_init(struct wd *ctx, struct WD_GD32VF103_FWDGT *base, clock_id_t clkid)
+{
+    ctx->base = base;
+    ctx->clkid = clkid;
+
+    return 0;
+}
+
+int wd_gd32vf103_fwdgt_setup(struct wd *ctx, struct wd_gd32vf103_fwdgt_settings *settings)
+{
+    unsigned long psc_p2;
+    clock_freq_t freq = clock_get_freq(ctx->clkid);
+    unsigned long nclk = (unsigned long)freq / 1000000ul * settings->timeout_us;
+
+    if (!picoRTOS_assert(freq > 0))
+        return -EIO;
+
+    (void)wd_stop(ctx);
+
+    for (psc_p2 = 2ul; psc_p2 <= 8ul; psc_p2++) {
+        unsigned long rld = nclk / (1ul << psc_p2);
+        if (rld <= (unsigned long)FWDGT_RLD_RLD_M) {
+            ctx->base->FWDGT_PSC = (uint32_t)FWDGT_PSC_PSC(psc_p2 - 2ul);
+            ctx->base->FWDGT_RLD = (uint32_t)FWDGT_RLD_RLD(rld);
+            return 0;
+        }
+    }
+
+    picoRTOS_break();
+    /*@notreached@*/ return -EINVAL;
+}
+
+int wd_start(struct wd *ctx)
+{
+    ctx->base->FWDGT_CTL = (uint32_t)CMD_START;
+    return 0;
+}
+
+int wd_stop(struct wd *ctx)
+{
+    ctx->base->FWDGT_CTL = (uint32_t)CMD_DISABLE;
+    return 0;
+}
+
+void wd_refresh(struct wd *ctx)
+{
+    ctx->base->FWDGT_CTL = (uint32_t)CMD_RELOAD;
+}
---
+++ staging/drivers/wd/wd-gd32vf103_fwdgt.h
@@ -0,0 +1,27 @@
+#ifndef WD_GD32VF103_FWDGT_H
+#define WD_GD32VF103_FWDGT_H
+
+#include "wd.h"
+#include "clock.h"
+
+struct WD_GD32VF103_FWDGT;
+
+struct wd {
+    /*@temp@*/ struct WD_GD32VF103_FWDGT *base;
+    clock_id_t clkid;
+};
+
+struct wd_gd32vf103_fwdgt_settings {
+    unsigned long timeout_us;
+};
+
+int wd_gd32vf103_fwdgt_init(/*@out@*/ struct wd *ctx, struct WD_GD32VF103_FWDGT *base, clock_id_t clkid);
+int wd_gd32vf103_fwdgt_setup(struct wd *ctx, struct wd_gd32vf103_fwdgt_settings *settings);
+
+/* Implements:
+ * int wd_start(struct wd *ctx);
+ * int wd_stop(struct wd *ctx);
+ * void wd_refresh(struct wd *ctx);
+ */
+
+#endif
