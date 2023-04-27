9c71e195691fac96b68cd1fe894ffaf301a7f0e0
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-04-27 15:19:45 +0200

    hal: flash HAL improvements

--- drivers/include/flash.h
+++ drivers/include/flash.h
@@ -6,29 +6,76 @@
 
 struct flash;
 
-/* Function: flash_erase_sector
+/* Function: flash_get_nblocks
+ * Gets the number of blocks present in the flash
+ *
+ * Parameters:
+ *  ctx - A flash instance
+ *
+ * Returns:
+ * The numbers of blocks if success, -errno otherwise
+ */
+int flash_get_nblocks(struct flash *ctx);
+
+/* Function: flash_get_erase_size
+ * Gets the minimal erase size for the specified block
+ *
+ * Parameters:
+ *  ctx - A flash instance
+ *  block - The block number
+ *
+ * Returns:
+ * The minimum erase size for this block if success, -errno otherwise
+ */
+int flash_get_erase_size(struct flash *ctx, size_t block);
+
+/* Function: flash_get_write_size
+ * Gets the minimal write size for the specified block
+ *
+ * Parameters:
+ *  ctx - A flash instance
+ *  block - The block number
+ *
+ * Returns:
+ * The minimum write size for this block if success, -errno otherwise
+ */
+int flash_get_write_size(struct flash *ctx, size_t block);
+
+/* Function: flash_get_block_addr
+ * Gets the memory address of the specified block
+ *
+ * Parameters:
+ *  ctx - A flash instance
+ *  block - The block number
+ *
+ * Returns:
+ * The address of the block if success, -errno otherwise
+ */
+int flash_get_block_addr(struct flash *ctx, size_t block);
+
+/* Function: flash_erase
  * Erases a sector from flash
  *
  * Parameters:
  *  ctx - A flash instance
- *  sector - The sector to erase
+ *  block - The block to erase
  *
  * Returns:
  * 0 in case of success, -errno otherwise
  */
-int flash_erase_sector(struct flash *ctx, size_t sector);
+int flash_erase(struct flash *ctx, size_t block);
 
-/* Function: flash_blankcheck_sector
- * Checks if a flash sector is blank/empty
+/* Function: flash_blankcheck
+ * Checks if a flash block is blank/empty
  *
  * Parameters:
  *  ctx - A flash instance
- *  sector - The sector to blankcheck
+ *  block - The block to blankcheck
  *
  * Returns:
  * 0 in case of success, -errno otherwise
  */
-int flash_blankcheck_sector(struct flash *ctx, size_t sector);
+int flash_blankcheck(struct flash *ctx, size_t block);
 
 /* Function: flash_write
  * Writes data to flash
@@ -44,28 +91,28 @@ int flash_blankcheck_sector(struct flash *ctx, size_t sector);
  */
 int flash_write(struct flash *ctx, size_t addr, const void *data, size_t n);
 
-/* Function: flash_lock_sector
- * Protects a sector in flash
+/* Function: flash_lock
+ * Protects a block in flash
  *
  * Parameters:
  *  ctx - A flash instance
- *  sector - The sector to protect
+ *  block - The block to protect
  *
  * Returns:
  * 0 if success, -errno otherwise
  */
-int flash_lock_sector(struct flash *ctx, size_t sector);
+int flash_lock(struct flash *ctx, size_t block);
 
-/* Function: flash_unlock_sector
- * Unprotects a flash sector
+/* Function: flash_unlock
+ * Unprotects a flash block
  *
  * Parameters:
  *  ctx - A flash instance
- *  sector - The sector to unprotect
+ *  block - The block to unprotect
  *
  * Returns:
  * 0 if success, -errno otherwise
  */
-int flash_unlock_sector(struct flash *ctx, size_t sector);
+int flash_unlock(struct flash *ctx, size_t block);
 
 #endif
