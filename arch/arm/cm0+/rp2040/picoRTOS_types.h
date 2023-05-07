0af77212d2327fba76e0d5bfc881437e3a227acd
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-05-07 13:20:10 +0200

    port: rp2040 support for dummy ARCH_L1_DCACHE_LINESIZE

--- arch/arm/cm0+/rp2040/picoRTOS_types.h
+++ arch/arm/cm0+/rp2040/picoRTOS_types.h
@@ -15,6 +15,9 @@ typedef unsigned long picoRTOS_cycles_t;
 #define ARCH_MIN_STACK_COUNT     (ARCH_INITIAL_STACK_COUNT + 4) /* in -O0 */
 #define ARCH_SMP_MIN_STACK_COUNT 0
 
+/* no cache */
+#define ARCH_L1_DCACHE_LINESIZE sizeof(unsigned long)
+
 /* splint doesn't like inline assembly */
 #ifdef S_SPLINT_S
 # define ASM(x) {}
