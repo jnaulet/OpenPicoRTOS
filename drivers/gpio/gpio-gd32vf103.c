1347ed783a24ca1261e39de0c7cf3bb92722a3d7
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-06-08 20:53:45 +0200

    gpio: unstaged gpio driver for gd32vf103, issue #40

--- drivers/gpio/gpio-gd32vf103.c
+++ drivers/gpio/gpio-gd32vf103.c
@@ -7,10 +7,22 @@ struct GPIO_GD32VF103 {
     volatile uint32_t GPIOx_CTLn[2];
     volatile uint32_t GPIOx_ISTAT;
     volatile uint32_t GPIOx_OCTL;
+    volatile uint32_t GPIOx_BOP;
     volatile uint32_t GPIOx_BC;
     volatile uint32_t GPIOx_LOCK;
 };
 
+/* Function: gpio_gd32vf103_init
+ * Initializes a GPIO
+ *
+ * Parameters:
+ *  ctx - The GPIO to init
+ *  base - The GPIO PORT base address
+ *  pin - The GPIO pin number on that PORT
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
 int gpio_gd32vf103_init(struct gpio *ctx, int base, size_t pin)
 {
     if (!picoRTOS_assert(pin < (size_t)GPIO_GD32VF103_PIN_COUNT)) return -EINVAL;
@@ -23,8 +35,8 @@ int gpio_gd32vf103_init(struct gpio *ctx, int base, size_t pin)
 
 void gpio_write(struct gpio *ctx, bool value)
 {
-    if (value) ctx->base->GPIOx_OCTL |= ctx->mask;
-    else ctx->base->GPIOx_OCTL &= ~ctx->mask;
+    if (value) ctx->base->GPIOx_BOP = ctx->mask;
+    else ctx->base->GPIOx_BC = ctx->mask;
 }
 
 bool gpio_read(struct gpio *ctx)
@@ -32,7 +44,7 @@ bool gpio_read(struct gpio *ctx)
     return (ctx->base->GPIOx_ISTAT & ctx->mask) != 0;
 }
 
-void gpio_toggle(/*@unused@*/ struct gpio *ctx __attribute__((unused)))
+void gpio_toggle(struct gpio *ctx)
 {
-    picoRTOS_break(); /* TBD */
+    ctx->base->GPIOx_OCTL ^= ctx->mask;
 }
