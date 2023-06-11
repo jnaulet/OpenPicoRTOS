1e6e45e8da1496ca3313946c53f2bc87578c8310
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-06-11 16:56:05 +0200

    wd: unstaged gd32vf103 fwdgt driver, issue #46

--- drivers/wd/wd-gd32vf103_fwdgt.c
+++ drivers/wd/wd-gd32vf103_fwdgt.c
@@ -20,6 +20,17 @@ struct WD_GD32VF103_FWDGT {
 #define FWDGT_RLD_RLD_M  0xfffu
 #define FWDGT_RLD_RLD(x) ((x) & FWDGT_RLD_RLD_M)
 
+/* Function: wd_gd32vf103_fwdgt_init
+ * Initializes a free watchdog timer
+ *
+ * Parameters:
+ *  ctx - The watchdog context to init
+ *  base - The WDT base address
+ *  clkid - The WDT input clock id
+ *
+ * Returns:
+ * Always 0
+ */
 int wd_gd32vf103_fwdgt_init(struct wd *ctx, int base, clock_id_t clkid)
 {
     ctx->base = (struct WD_GD32VF103_FWDGT*)base;
@@ -28,20 +39,29 @@ int wd_gd32vf103_fwdgt_init(struct wd *ctx, int base, clock_id_t clkid)
     return 0;
 }
 
+/* Function: wd_gd32vf103_fwdgt_setup
+ * Configures a watchdog
+ *
+ * Parameters:
+ *  ctx - The watchdog to configure
+ *  settings - The parameters to set
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
 int wd_gd32vf103_fwdgt_setup(struct wd *ctx, struct wd_gd32vf103_fwdgt_settings *settings)
 {
     unsigned long psc_p2;
     clock_freq_t freq = clock_get_freq(ctx->clkid);
-    unsigned long nclk = (unsigned long)freq / 1000000ul * settings->timeout_us;
+    unsigned long nclk = (unsigned long)freq * settings->timeout_us / 1000000ul;
 
     if (!picoRTOS_assert(freq > 0))
         return -EIO;
 
-    (void)wd_stop(ctx);
-
     for (psc_p2 = 2ul; psc_p2 <= 8ul; psc_p2++) {
         unsigned long rld = nclk / (1ul << psc_p2);
         if (rld <= (unsigned long)FWDGT_RLD_RLD_M) {
+            ctx->base->FWDGT_CTL = (uint32_t)CMD_DISABLE;
             ctx->base->FWDGT_PSC = (uint32_t)FWDGT_PSC_PSC(psc_p2 - 2ul);
             ctx->base->FWDGT_RLD = (uint32_t)FWDGT_RLD_RLD(rld);
             return 0;
