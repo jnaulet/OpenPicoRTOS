023555fec0ece6bbb7a197a623279704861ce2eb
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-07-03 16:40:25 +0200

    gpio: support for gpio on stm32h7xx family (staging)

---
+++ staging/drivers/gpio/gpio-stm32h7xx.c
@@ -0,0 +1,64 @@
+#include "gpio-stm32h7xx.h"
+#include "picoRTOS.h"
+
+#include <stdint.h>
+
+struct GPIO_STM32H7XX {
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDR;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t LCKR;
+    volatile uint32_t AFRL;
+    volatile uint32_t AFRH;
+};
+
+int gpio_stm32h7xx_init(struct gpio *ctx, int base, size_t pin)
+{
+    if (!picoRTOS_assert(pin < (size_t)GPIO_STM32H7XX_PIN_COUNT)) return -EINVAL;
+
+    ctx->base = (struct GPIO_STM32H7XX*)base;
+    ctx->mask = (uint32_t)(1 << pin);
+    ctx->invert = false;
+
+    return 0;
+}
+
+static int set_invert(struct gpio *ctx, gpio_invert_t invert)
+{
+    if (!picoRTOS_assert(invert != GPIO_INVERT_IGNORE)) return -EINVAL;
+    if (!picoRTOS_assert(invert < GPIO_INVERT_COUNT)) return -EINVAL;
+
+    ctx->invert = (invert == GPIO_INVERT_ENABLE);
+    return 0;
+}
+
+int gpio_setup(struct gpio *ctx, struct gpio_settings *settings)
+{
+    int res;
+
+    if (settings->invert != GPIO_INVERT_IGNORE &&
+        (res = set_invert(ctx, settings->invert)) < 0)
+        return res;
+
+    return 0;
+}
+
+void gpio_write(struct gpio *ctx, bool value)
+{
+    if (value ^ ctx->invert) ctx->base->BSRR = ctx->mask;
+    else ctx->base->BSRR = ctx->mask << 16;
+}
+
+bool gpio_read(struct gpio *ctx)
+{
+    return ((ctx->base->IDR & ctx->mask) != 0) ^ ctx->invert;
+}
+
+void gpio_toggle(struct gpio *ctx)
+{
+    ctx->base->ODR ^= ctx->mask;
+}
