3e6e6b6944c8ea3abe5422fb795c10ad5aa92da8
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-07-30 13:17:29 +0200

    drviers: unstaged support for stm32h7xx gpios

--- drivers/gpio/gpio-stm32h7xx.c
+++ drivers/gpio/gpio-stm32h7xx.c
@@ -16,6 +16,17 @@ struct GPIO_STM32H7XX {
     volatile uint32_t AFRH;
 };
 
+/* Function: gpio_stm32h7xx_init
+ * Initializes a GPIO
+ *
+ * Parameters:
+ *  ctx - The GPIO to init
+ *  base - The GPIO PORT base address
+ *  pin - The GPIO pin number on that PORT
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
 int gpio_stm32h7xx_init(struct gpio *ctx, int base, size_t pin)
 {
     if (!picoRTOS_assert(pin < (size_t)GPIO_STM32H7XX_PIN_COUNT)) return -EINVAL;
