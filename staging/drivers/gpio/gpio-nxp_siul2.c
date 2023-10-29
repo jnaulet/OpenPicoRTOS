20f9c528e4014461c81534a56d35f7e60ffd9a7d
Joachim Naulet <jnaulet@rdinnovation.fr>
2023-10-29 21:22:59 +0100

    gpio: support for gpio_setup on NXP SIUL2

--- staging/drivers/gpio/gpio-nxp_siul2.c
+++ staging/drivers/gpio/gpio-nxp_siul2.c
@@ -17,22 +17,43 @@ int gpio_nxp_siul2_init(struct gpio *ctx, int base, size_t pin)
 
     ctx->base = (struct GPIO_NXP_SIUL2*)base;
     ctx->mask = (uint16_t)(0x8000u >> pin);
+    ctx->invert = false;
+
+    return 0;
+}
+
+static int set_invert(struct gpio *ctx, gpio_invert_t invert)
+{
+    picoRTOS_assert(invert != GPIO_INVERT_IGNORE, return -EINVAL);
+    picoRTOS_assert(invert < GPIO_INVERT_COUNT, return -EINVAL);
+
+    ctx->invert = (invert == GPIO_INVERT_ENABLE);
+    return 0;
+}
+
+int gpio_setup(struct gpio *ctx, struct gpio_settings *settings)
+{
+    int res;
+
+    if (settings->invert != GPIO_INVERT_IGNORE &&
+        (res = set_invert(ctx, settings->invert)) < 0)
+        return res;
 
     return 0;
 }
 
 void gpio_write(struct gpio *ctx, bool value)
 {
-    if (value) ctx->base->PGPDO |= ctx->mask;
+    if (value ^ ctx->invert) ctx->base->PGPDO |= ctx->mask;
     else ctx->base->PGPDO &= ~ctx->mask;
 }
 
 bool gpio_read(struct gpio *ctx)
 {
-    return (ctx->base->PGPDI & ctx->mask) != 0;
+    return ((ctx->base->PGPDI & ctx->mask) != 0) ^ ctx->invert;
 }
 
-void gpio_toggle(__attribute__((unused)) /*@unused@*/ struct gpio *ctx)
+void gpio_toggle(struct gpio *ctx)
 {
-    picoRTOS_break(); /* unsupported */
+    ctx->base->PGPDO ^= ctx->mask;
 }
--- staging/drivers/gpio/gpio-nxp_siul2.h
+++ staging/drivers/gpio/gpio-nxp_siul2.h
@@ -10,6 +10,7 @@ struct GPIO_NXP_SIUL2;
 struct gpio {
     /*@temp@*/ struct GPIO_NXP_SIUL2 *base;
     uint16_t mask;
+    bool invert;
 };
 
 #define GPIO_NXP_SIUL2_PIN_COUNT 16
@@ -17,6 +18,7 @@ struct gpio {
 int gpio_nxp_siul2_init(/*@out@*/ struct gpio *ctx, int base, size_t pin);
 
 /* Runtime calls:
+ * int gpio_setup(struct gpio *ctx, struct gpio_settings *settings);
  * void gpio_write(struct gpio *ctx, bool value);
  * bool gpio_read(struct gpio *ctx);
  * void gpio_toggle(struct gpio *ctx);
