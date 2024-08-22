7cd2e1b0216986bfa19c9e7b2a652b00796bf4fc
David J. Fiddes <D.J@fiddes.net>
2024-08-22 11:54:06 +0100

    Fix ncurses probe when using gcc 14.x
    It seems that gcc 14.2.1 fails with a hard error if main()
    does not have an int return type. This causes a hard
    error when probing for ncurses. Adding the return type
    fixes "make menuconfig" with host gcc 14.2.1 on
    Fedora 40.

--- scripts/kconfig/lxdialog/check-lxdialog.sh
+++ scripts/kconfig/lxdialog/check-lxdialog.sh
@@ -40,7 +40,7 @@ trap "rm -f $tmp" 0 1 2 3 15
 check() {
         $cc -xc - -o $tmp 2>/dev/null <<'EOF'
 #include CURSES_LOC
-main() {}
+int main() {}
 EOF
 	if [ $? != 0 ]; then
 	    echo " *** Unable to find the ncurses libraries or the"       1>&2
