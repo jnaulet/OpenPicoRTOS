fcafdbfa97417b056c2da05ccb1c345f1c038dff
Joachim Naulet <jnaulet@rdinnovation.fr>
2024-10-03 16:51:04 +0200

    drivers: mux-wch_ch32x shift fix (staging)

--- staging/drivers/mux/mux-wch_ch32x.c
+++ staging/drivers/mux/mux-wch_ch32x.c
@@ -57,12 +57,12 @@ int mux_wch_ch32x_input(struct mux *ctx, size_t pin, mux_wch_ch32x_input_t type)
     picoRTOS_assert(type < MUX_WCH_CH32X_INPUT_COUNT, return -EINVAL);
 
     if (pin < (size_t)8) {
-        size_t shift = pin << 4;
+        size_t shift = pin << 2;
         ctx->base->CFGLR &= ~(CFGLR_CNF0(CFGLR_CNF0_M) << shift);
         ctx->base->CFGLR |= (CFGLR_CNF0(type) << shift);
         ctx->base->CFGLR &= ~(CFGLR_MODE0(CFGLR_MODE0_M) << shift);
     }else{
-        size_t shift = (pin - (size_t)8) << 4;
+        size_t shift = (pin - (size_t)8) << 2;
         ctx->base->CFGHR &= ~(CFGHR_CNF8(CFGHR_CNF8_M) << shift);
         ctx->base->CFGHR |= (CFGHR_CNF8(type) << shift);
         ctx->base->CFGHR &= ~(CFGHR_MODE8(CFGHR_MODE8_M) << shift);
