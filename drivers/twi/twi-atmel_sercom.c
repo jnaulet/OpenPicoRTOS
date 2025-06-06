c686df0ee950e4d60ef6f93a02011413dcccd5a2
Joachim Naulet <jnaulet@rdinnovation.fr>
2025-06-06 17:46:17 +0200

    twi-atmel_sercom: migrated to new api

--- drivers/twi/twi-atmel_sercom.c
+++ drivers/twi/twi-atmel_sercom.c
@@ -307,9 +307,12 @@ static int twi_write_as_master_data(struct twi *ctx, const void *buf, size_t n)
     return 1;
 }
 
-static int twi_write_as_master(struct twi *ctx, const void *buf, size_t n)
+static int twi_write_as_master(struct twi *ctx, const void *buf, size_t n, int flags)
 {
     picoRTOS_assert(n > 0, return -EINVAL);
+
+    /* FIXME */
+    /*@i@*/ (void)flags;
 
     /* NACK */
     if ((ctx->base->STATUS & STATUS_RXNACK) != 0)
@@ -391,12 +394,12 @@ static int twi_write_as_slave(struct twi *ctx, const void *buf, size_t n)
     /*@notreached@*/ return -EIO;
 }
 
-int twi_write(struct twi *ctx, const void *buf, size_t n)
+int twi_write(struct twi *ctx, const void *buf, size_t n, int flags)
 {
     picoRTOS_assert(n > 0, return -EINVAL);
 
     if (ctx->mode == TWI_MODE_MASTER)
-        return twi_write_as_master(ctx, buf, n);
+        return twi_write_as_master(ctx, buf, n, flags);
 
     if (ctx->mode == TWI_MODE_SLAVE)
         return twi_write_as_slave(ctx, buf, n);
@@ -449,9 +452,12 @@ static int twi_read_as_master_data(struct twi *ctx, void *buf, size_t n)
     return 1;
 }
 
-static int twi_read_as_master(struct twi *ctx, void *buf, size_t n)
+static int twi_read_as_master(struct twi *ctx, void *buf, size_t n, int flags)
 {
     picoRTOS_assert(n > 0, return -EINVAL);
+
+    /* FIXME */
+    /*@i@*/ (void)flags;
 
     /* NACK */
     if ((ctx->base->STATUS & STATUS_RXNACK) != 0)
@@ -527,12 +533,12 @@ static int twi_read_as_slave(struct twi *ctx, void *buf, size_t n)
     /*@notreached@*/ return -EIO;
 }
 
-int twi_read(struct twi *ctx, void *buf, size_t n)
+int twi_read(struct twi *ctx, void *buf, size_t n, int flags)
 {
     picoRTOS_assert(n > 0, return -EINVAL);
 
     if (ctx->mode == TWI_MODE_MASTER)
-        return twi_read_as_master(ctx, buf, n);
+        return twi_read_as_master(ctx, buf, n, flags);
 
     if (ctx->mode == TWI_MODE_SLAVE)
         return twi_read_as_slave(ctx, buf, n);
