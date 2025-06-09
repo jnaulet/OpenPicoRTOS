112ed780c857343e7a3eb7ccb0725bdd5694537b
Joachim Naulet <jnaulet@rdinnovation.fr>
2025-06-09 21:23:11 +0200

    distcheck: remove amigaball-lcd from general staticcheck

--- test/run_demo_build.sh
+++ test/run_demo_build.sh
@@ -7,6 +7,7 @@ for d in $(find $DIR -mindepth 1 -maxdepth 1 -type d); do
     # exclude these
     # [ ! -z $(echo $d | grep pthread-linux) ] && continue
     # [ ! -z $(echo $d | grep win32) ] && continue
+    [ ! -z $(echo $d | grep amigaball-lcd) ] && continue
     
     cd $d && make V=1 || exit 1
 done
--- test/run_demo_staticcheck.sh
+++ test/run_demo_staticcheck.sh
@@ -13,6 +13,7 @@ for d in $(find $DIR -mindepth 1 -maxdepth 1 -type d); do
     [ ! -z $(echo $d | grep win32) ] && continue
     # FIXME: EXTRA_CFLAGS issue
     [ ! -z $(echo $d | grep pic32mz) ] && continue
+    [ ! -z $(echo $d | grep amigaball-lcd) ] && continue
     
     cd $d && make staticcheck V=1 || exit 1
 done
