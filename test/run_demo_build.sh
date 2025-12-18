ec9a8a878885ee71e396cc6c07a6befceb716fa8
Joachim Naulet <jnaulet@rdinnovation.fr>
2025-12-18 11:44:00 +0100

    test: removed hard to get commercial toolchains from tests

--- test/run_demo_build.sh
+++ test/run_demo_build.sh
@@ -8,6 +8,9 @@ for d in $(find $DIR -mindepth 1 -maxdepth 1 -type d); do
     # [ ! -z $(echo $d | grep pthread-linux) ] && continue
     # [ ! -z $(echo $d | grep win32) ] && continue
     [ ! -z $(echo $d | grep amigaball-lcd) ] && continue
+    # commercial toolchains
+    [ ! -z $(echo $d | grep devkit-mpc5748g) ] && continue
+    [ ! -z $(echo $d | grep curiosity-2.0-pic32mz-ef) ] && continue
     
     cd $d && make V=1 || exit 1
 done
--- test/run_staging_demo_build.sh
+++ test/run_staging_demo_build.sh
@@ -7,6 +7,8 @@ for d in $(find $DIR -mindepth 1 -maxdepth 1 -type d); do
     # exclude these
     # [ ! -z $(echo $d | grep pthread-linux) ] && continue
     # [ ! -z $(echo $d | grep win32) ] && continue
+    # commercial toolchains
+    [ ! -z $(echo $d | grep mpc5775e-evb) ] && continue
     
     cd $d && make V=1 || exit 1
 done
