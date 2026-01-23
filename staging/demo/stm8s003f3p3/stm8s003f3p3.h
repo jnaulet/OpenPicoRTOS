ff5aa7eceb145d2c10d62cb87b7ecdadb00ae8f8
Joachim Naulet <jnaulet@rdinnovation.fr>
2026-01-23 08:30:18 +0100

    demo: uncrustify pass on stm8

--- staging/demo/stm8s003f3p3/stm8s003f3p3.h
+++ staging/demo/stm8s003f3p3/stm8s003f3p3.h
@@ -4,8 +4,8 @@
 #include "gpio-stm8.h"
 
 struct stm8s003f3p3 {
-  struct gpio TICK;
-  struct gpio TEST;
+    struct gpio TICK;
+    struct gpio TEST;
 };
 
 int stm8s003f3p3_init(/*@out@*/ struct stm8s003f3p3 *ctx);
