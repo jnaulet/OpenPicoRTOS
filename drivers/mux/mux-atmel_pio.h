2933812571b64d1acc15e9273c7e2e036030d69e
Joachim Naulet <jnaulet@rdinnovation.fr>
2026-03-19 09:49:21 +0100

    mux: support for more generic Atmel PIO mux IP

---
+++ drivers/mux/mux-atmel_pio.h
@@ -0,0 +1,29 @@
+#ifndef MUX_ATMEL_PIO_H
+#define MUX_ATMEL_PIO_H
+
+#include "mux.h"
+
+struct MUX_ATMEL_PIO;
+
+#define MUX_ATMEL_PIN_COUNT 32
+
+struct mux {
+    /*@temp@*/ struct MUX_ATMEL_PIO *base;
+};
+
+typedef enum {
+    MUX_ATMEL_PIO_DISABLE,
+    MUX_ATMEL_PIO_GPIO,
+    MUX_ATMEL_PIO_A,
+    MUX_ATMEL_PIO_B,
+    MUX_ATMEL_PIO_COUNT
+} mux_atmel_pio_t;
+
+int mux_atmel_pio_init(/*@out@*/ struct mux *ctx, int base);
+
+int mux_atmel_pio_input(struct mux *ctx, size_t pin, mux_atmel_pio_t mux);
+int mux_atmel_pio_output(struct mux *ctx, size_t pin, mux_atmel_pio_t mux);
+
+int mux_atmel_pio_pull_up(struct mux *ctx, size_t pin);
+
+#endif
