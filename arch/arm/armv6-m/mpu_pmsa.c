157ea4f961d0b215698236c8fd06b716dc046321
Joachim Naulet <jnaulet@rdinnovation.fr>
2026-06-02 22:45:16 +0200

    mpu: some improvement on arm pmsav6

--- arch/arm/armv6-m/mpu_pmsa.c
+++ arch/arm/armv6-m/mpu_pmsa.c
@@ -77,11 +77,11 @@ struct mpu_entry {
 static struct {
     /* global (pre-sheduler) */
     size_t dregion;
-    struct mpu_entry MPU[MPU_PMSAV7_DREGION_COUNT];
+    struct mpu_entry MPU[MPU_PMSAV6_DREGION_COUNT];
     size_t count;
     /* by process */
     struct mpu_pid {
-        struct mpu_entry MPU[MPU_PMSAV7_DREGION_COUNT];
+        struct mpu_entry MPU[MPU_PMSAV6_DREGION_COUNT];
         size_t count;
     } pid[TASK_COUNT];
 } mpu;
@@ -93,12 +93,11 @@ void arch_mpu_init(void)
     size_t n;
 
     mpu.dregion = (size_t)((MPU->MPU_TYPE >> MPU_TYPE_DREGION_S) & MPU_TYPE_DREGION_M);
-    mpu.count = 0;
-
     arch_assert(mpu.dregion == (size_t)MPU_PMSAV6_DREGION_COUNT, return );
 
     /* force reset */
-    for (n = 0; n < mpu.dregion; n++) {
+    mpu.count = mpu.dregion;
+    for (n = 0; n < mpu.count; n++) {
         MPU->MPU_RNR = (uint32_t)MPU_RNR_REGION(n);
         MPU->MPU_RASR = 0;
         /* init processes */
@@ -114,7 +113,7 @@ void arch_mpu_init(void)
     arch_mpu_add_region(PID_KERNEL, (void*)SCS_BASE, (size_t)SCS_LEN, 0xeu);
 }
 
-static int parse_mode(mpu_mode_t mode)
+static uint32_t RASR_from_mode(mpu_mode_t mode)
 {
     /* defaults: non-executable, privileged read-only, normal cacheable */
     uint32_t MPU_RASR = (uint32_t)(MPU_RASR_XN | MPU_RASR_AP(5) |
@@ -138,12 +137,12 @@ static int parse_mode(mpu_mode_t mode)
             MPU_RASR |= MPU_RASR_AP(1);
     }
 
-    return (int)MPU_RASR;
+    return MPU_RASR;
 }
 
 #define MPU_ENTRY_MATCH(entry, addr, n, mode)                           \
   ((uintptr_t)addr >= (entry)->match.addr &&                            \
-   (uintptr_t)addr + n <= (entry)->match.addr + n &&                    \
+   (uintptr_t)addr + n <= (entry)->match.addr + (entry)->match.n &&     \
    (mode) == (entry)->match.mode) /* FIXME, some mode should be compatible */
 
 static int region_already_exists(int pid, const void *addr, size_t n, mpu_mode_t mode)
@@ -152,108 +151,166 @@ static int region_already_exists(int pid, const void *addr, size_t n, mpu_mode_t
     arch_assert(pid >= PID_KERNEL, return -EINVAL);
     arch_assert(n > 0, return -EINVAL);
 
-    size_t i;
+    size_t i = (size_t)MPU_PMSAV6_DREGION_COUNT;
 
     /* try to match global */
-    for (i = 0; i < mpu.count; i++)
+    while (i-- != mpu.count)
         if (MPU_ENTRY_MATCH(&mpu.MPU[i], addr, n, mode)) return (int)i;
     /* try to match pid */
-    for (; i < mpu.pid[pid].count; i++)
+    for (i = mpu.count; i-- != 0;)
         if (MPU_ENTRY_MATCH(&mpu.pid[pid].MPU[i], addr, n, mode)) return (int)i;
 
     return -ENOENT;
 }
 
+static uint32_t RBAR_SIZE_from_addr_n(uintptr_t addr, size_t n,
+                                      /*@reldef@*/ uintptr_t *rRBAR)
+{
+#define MPU_RASR_SIZE_COUNT 31 /* up to 32 in theory */
+    arch_assert(n > 0, return 0);
+
+    uint32_t SIZE;
+    uintptr_t RBAR = addr;
+
+    /* find closest power of 2 */
+    for (SIZE = (uint32_t)7; SIZE < (uint32_t)MPU_RASR_SIZE_COUNT; SIZE++) {
+        size_t size = (size_t)(1u << (SIZE + 1));
+        /* align addr & fix len */
+        RBAR = (uintptr_t)((size_t)RBAR & ~(size - 1));
+        size_t n1 = n + (size_t)((uintptr_t)addr - RBAR);
+        if (n1 <= size) break;
+    }
+
+    /*
+     * No fine tuning on armv6-m as we're already short on entries
+     */
+    *rRBAR = RBAR;
+    return SIZE;
+}
+
+static uint32_t SRD_from_RBAR_SIZE(uintptr_t addr, size_t n,
+                                   uintptr_t RBAR, uint32_t SIZE)
+{
+    arch_assert(n > 0, return 0);
+
+    /* fine-tune with SRD */
+    size_t size = (size_t)(1u << (SIZE + 1));
+    size_t sr_size = size / (size_t)8;
+    size_t pre = (size_t)(addr - RBAR);
+    size_t post = (size_t)((RBAR + size) - (addr + n));
+    /* last segment will be cut short if needed */
+    uint32_t SRD = ((uint32_t)0xff00u >> (post / sr_size) |
+                    (uint32_t)0x00ffu >> (8u - (pre / sr_size)));
+
+    /* for regions < 256 bytes */
+    if (size < (size_t)256)
+        SRD = 0;
+
+    return SRD;
+}
+
+static void addr_n_from_RBAR_SIZE_SRD(uintptr_t RBAR, uint32_t SIZE, uint32_t SRD,
+                                      /*@reldef@*/ uintptr_t *raddr,
+                                      /*@reldef@*/ uintptr_t *rn)
+{
+    size_t i;
+    size_t n = (size_t)(1u << (SIZE + 1));
+    size_t sr_size = n >> 3;
+
+    for (i = (size_t)8; i-- != 0;)
+        if ((SRD & (uint32_t)(1u << i)) != 0) n -= sr_size;
+
+    for (i = 0; i < (size_t)8; i++) {
+        if ((SRD & (uint32_t)(1u << i)) == 0) break;
+        RBAR += sr_size;
+    }
+
+    *raddr = RBAR;
+    *rn = n;
+}
+
 void arch_mpu_add_region(int pid, const void *addr, size_t n, mpu_mode_t mode)
 {
-#define MPU_RASR_SIZE_COUNT 31 /* 32 is supported but unrealistic */
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-    arch_assert(n > 0, return );
     arch_assert(pid < TASK_COUNT, return );
     arch_assert(pid >= PID_KERNEL, return );
 
     /* jump out asap */
-    if (region_already_exists(pid, addr, n, mode) > -1)
+    if (n == 0 || region_already_exists(pid, addr, n, mode) > -1)
         return;
 
+    size_t index;
+    struct mpu_entry *entry;
     bool is_kernel = (pid < 0);
-    uintptr_t MPU_RBAR = (uintptr_t)addr;
-    uint32_t MPU_RASR = (uint32_t)parse_mode(mode);
+    uintptr_t start = (uintptr_t)addr;
+    uint32_t MPU_RASR = RASR_from_mode(mode);
 
-    while (n != 0) {
-
-        size_t size = 0;
-        uint32_t SIZE = 0;
-        struct mpu_entry *mpu_entry;
-        size_t index = mpu.count + (is_kernel ? 0 : mpu.pid[pid].count);
-
+    if (is_kernel) {
+        index = mpu.count - 1;
         arch_assert(index < (size_t)MPU_PMSAV6_DREGION_COUNT, return );
-
-        /* find closest power of 2 */
-        for (SIZE = (uint32_t)7; SIZE < (uint32_t)MPU_RASR_SIZE_COUNT; SIZE++) {
-            size = (size_t)(1u << (SIZE + 1));
-            /* align addr & fix len */
-            MPU_RBAR = (uintptr_t)((size_t)MPU_RBAR & ~(size - 1));
-            size_t n1 = n + (size_t)((uintptr_t)addr - MPU_RBAR);
-            if (n1 <= size) /*@innerbreak@*/ break;
+    }else{
+        index = mpu.pid[pid].count;
+        /*
+         * The ARMv6-M has very limited resources and we exhausted them.
+         * Show must go on, we sacrifice this tasks' safety net but
+         * still preserve the kernel space, at least
+         */
+        if (index == mpu.count) {
+            entry = &mpu.pid[pid].MPU[--index];
+            entry->MPU_RBAR = (uint32_t)(MPU_RBAR_REGION(index) | MPU_RBAR_VALID);
+            entry->MPU_RASR = (uint32_t)(MPU_RASR | MPU_RASR_SRD(0) |
+                                         MPU_RASR_SIZE(MPU_RASR_SIZE_M) |
+                                         MPU_RASR_ENABLE);
+            /* go back */
+            arch_mpu_restore_regions(pid);
+            return;
         }
-
-        /* fine-tune with SRD */
-        size_t sr_size = size >> 3;
-        size_t pre = (size_t)((uintptr_t)addr - MPU_RBAR);
-        size_t post = (size_t)((MPU_RBAR + (uintptr_t)size) - ((uintptr_t)addr + (uintptr_t)n));
-        uint32_t SRD = ((uint32_t)0xff00u >> (post / sr_size) |
-                        (uint32_t)0x00ffu >> (8u - (pre / sr_size)));
-
-        /* for regions < 256 bytes */
-        if (size < (size_t)256)
-            SRD = 0;
-
-        /* set reg */
-        MPU->MPU_RNR = (uint32_t)MPU_RNR_REGION(index);
-        MPU->MPU_RBAR = (uint32_t)MPU_RBAR;
-        MPU->MPU_RASR = (uint32_t)(MPU_RASR | MPU_RASR_SRD(SRD) |
-                                   MPU_RASR_SIZE(SIZE) | MPU_RASR_ENABLE);
-
-        if (is_kernel) {
-            mpu_entry = &mpu.MPU[index];
-            mpu.count++;
-        }else{
-            mpu_entry = &mpu.pid[pid].MPU[index];
-            mpu.pid[pid].count++;
-        }
-
-        /* fill-up entry */
-        mpu_entry->MPU_RBAR = MPU->MPU_RBAR | MPU_RBAR_VALID;
-        mpu_entry->MPU_RASR = MPU->MPU_RASR;
-        /* cache values */
-        mpu_entry->match.addr = MPU_RBAR;
-        mpu_entry->match.n = size;
-        mpu_entry->match.mode = mode;
-
-        /* prepare next round */
-        n -= MIN(n, size);
-        MPU_RBAR += (uintptr_t)size;
     }
+
+    uintptr_t MPU_RBAR = 0;
+    uint32_t SIZE = RBAR_SIZE_from_addr_n(start, n, &MPU_RBAR);
+    uint32_t SRD = SRD_from_RBAR_SIZE(start, n, MPU_RBAR, SIZE);
+
+    /* set reg */
+    MPU->MPU_RNR = (uint32_t)MPU_RNR_REGION(index);
+    MPU->MPU_RBAR = (uint32_t)MPU_RBAR;
+    MPU->MPU_RASR = (uint32_t)(MPU_RASR | MPU_RASR_SRD(SRD) |
+                               MPU_RASR_SIZE(SIZE) | MPU_RASR_ENABLE);
+
+    /* kernel regions are put last (highest priority) */
+    if (is_kernel) entry = &mpu.MPU[--mpu.count];
+    else entry = &mpu.pid[pid].MPU[mpu.pid[pid].count++];
+
+    /* fill-up entry */
+    entry->MPU_RBAR = MPU->MPU_RBAR | MPU_RBAR_VALID;
+    entry->MPU_RASR = MPU->MPU_RASR;
+    /* cache values */
+    entry->match.mode = mode;
+    addr_n_from_RBAR_SIZE_SRD(MPU_RBAR, SIZE, SRD,
+                              &entry->match.addr,
+                              &entry->match.n);
+
+    /* no checks on armv6-m */
 }
 
 void arch_mpu_restore_regions(int pid)
 {
-    size_t n;
-
     arch_assert(pid < TASK_COUNT, return );
     arch_assert(pid >= PID_KERNEL, return );
 
-    if (pid < 0) {
+    bool is_kernel = (pid < 0);
+
+    if (is_kernel) {
         /* only used for SMP */
-        for (n = 0; n < (size_t)MPU_PMSAV6_DREGION_COUNT; n++) {
+        size_t n = (size_t)MPU_PMSAV6_DREGION_COUNT;
+        while (n-- != mpu.count) {
             MPU->MPU_RBAR = mpu.MPU[n].MPU_RBAR;
             MPU->MPU_RASR = mpu.MPU[n].MPU_RASR;
         }
     }else{
+        size_t n = mpu.count;
         struct mpu_pid *p = &mpu.pid[pid];
-        for (n = mpu.count; n < (size_t)MPU_PMSAV6_DREGION_COUNT; n++) {
+
+        while (n-- != 0) {
             MPU->MPU_RBAR = p->MPU[n].MPU_RBAR;
             MPU->MPU_RASR = p->MPU[n].MPU_RASR;
         }
