4015a34a21094fe384afd23610e98412e402f3a5
Joachim Naulet <jnaulet@rdinnovation.fr>
2026-06-08 14:05:09 +0200

    gpio: added claim() interface to nxp siul2 driver

--- drivers/gpio/gpio-nxp_siul2.c
+++ drivers/gpio/gpio-nxp_siul2.c
@@ -1,5 +1,7 @@
 #include "gpio-nxp_siul2.h"
+
 #include "picoRTOS.h"
+#include "picoRTOS_port.h"
 
 #include <stdint.h>
 
@@ -7,11 +9,27 @@ struct GPIO_NXP_SIUL2 {
     volatile uint16_t PGPDO;
     uint16_t RESERVED0[31];
     volatile uint16_t PGPDI;
-    uint16_t RESERVED1[31];
-    volatile uint32_t MPGPDO;
+    /* uint16_t RESERVED1[31]; */
+    /* volatile uint32_t MPGPDO; */
 };
 
 /* Function: gpio_nxp_siul2_init
+ * Initializes a GPIO block
+ *
+ * Parameters:
+ *  ctx - The GPIO block to init
+ *  base - The GPIO block base address
+ *
+ * Returns:
+ * 0 if success, -errno otherwise
+ */
+int gpio_nxp_siul2_init(struct gpio_nxp_siul2 *ctx, int base)
+{
+    ctx->base = (struct GPIO_NXP_SIUL2*)base; // NOLINT
+    return 0;
+}
+
+/* Function: gpio_nxp_siul2_gpio_init
  * Initializes a GPIO
  *
  * Parameters:
@@ -22,11 +40,11 @@ struct GPIO_NXP_SIUL2 {
  * Returns:
  * 0 if success, -errno otherwise
  */
-int gpio_nxp_siul2_init(struct gpio *ctx, int base, size_t pin)
+int gpio_nxp_siul2_gpio_init(struct gpio *ctx, struct gpio_nxp_siul2 *parent, size_t pin)
 {
     picoRTOS_assert(pin < (size_t)GPIO_NXP_SIUL2_PIN_COUNT, return -EINVAL);
 
-    ctx->base = (struct GPIO_NXP_SIUL2*)base; // NOLINT
+    ctx->parent = parent;
     ctx->mask = (uint16_t)(0x8000u >> pin);
     ctx->invert = false;
 
@@ -55,16 +73,25 @@ int gpio_setup(struct gpio *ctx, const struct gpio_settings *settings)
 
 void gpio_write(struct gpio *ctx, bool value)
 {
-    if (value ^ ctx->invert) ctx->base->PGPDO |= ctx->mask;
-    else ctx->base->PGPDO &= ~ctx->mask;
+    if (value ^ ctx->invert) ctx->parent->base->PGPDO |= ctx->mask;
+    else ctx->parent->base->PGPDO &= ~ctx->mask;
 }
 
 bool gpio_read(struct gpio *ctx)
 {
-    return ((ctx->base->PGPDI & ctx->mask) != 0) ^ ctx->invert;
+    return ((ctx->parent->base->PGPDI & ctx->mask) != 0) ^ ctx->invert;
 }
 
 void gpio_toggle(struct gpio *ctx)
 {
-    ctx->base->PGPDO ^= ctx->mask;
+    ctx->parent->base->PGPDO ^= ctx->mask;
+}
+
+struct gpio *gpio_claim(struct gpio *ctx)
+{
+    /* FIXME: the GPIOs are interweaved on SIUL2 */
+    picoRTOS_mpu_add_region(ctx->parent, sizeof(*ctx->parent), MM_URW);
+    picoRTOS_mpu_add_region(ctx->parent->base, sizeof(*ctx->parent->base),
+                            MM_URW | MM_NON_CACHEABLE);
+    return ctx;
 }
--- drivers/gpio/gpio-nxp_siul2.h
+++ drivers/gpio/gpio-nxp_siul2.h
@@ -1,27 +1,34 @@
 #ifndef GPIO_NXP_SIUL2_H
 #define GPIO_NXP_SIUL2_H
 
-#include "gpio.h"
+#include "gpio+safety.h"
 #include <stdint.h>
 #include <stddef.h>
 
 struct GPIO_NXP_SIUL2;
 
-struct gpio {
+struct gpio_nxp_siul2 {
     /*@temp@*/ struct GPIO_NXP_SIUL2 *base;
-    uint16_t mask;
-    bool invert;
 };
 
 #define GPIO_NXP_SIUL2_PIN_COUNT 16
 
-int gpio_nxp_siul2_init(/*@out@*/ struct gpio *ctx, int base, size_t pin);
+int gpio_nxp_siul2_init(/*@out@*/ struct gpio_nxp_siul2 *ctx, int base);
+
+struct gpio {
+    /*@temp@*/ struct gpio_nxp_siul2 *parent;
+    uint16_t mask;
+    bool invert;
+};
+
+int gpio_nxp_siul2_gpio_init(/*@out@*/ struct gpio *ctx, struct gpio_nxp_siul2 *parent, size_t pin);
 
 /* Runtime calls:
  * int gpio_setup(struct gpio *ctx, struct gpio_settings *settings);
  * void gpio_write(struct gpio *ctx, bool value);
  * bool gpio_read(struct gpio *ctx);
  * void gpio_toggle(struct gpio *ctx);
+ * struct gpio *gpio_claim(struct gpio *ctx);
  */
 
 #endif
