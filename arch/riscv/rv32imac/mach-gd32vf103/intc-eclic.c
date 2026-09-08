5fbadbd58af10a75788b071a3cc8baca5f2f8c8f
Joachim Naulet <jnaulet@rdinnovation.fr>
2026-09-08 22:48:55 +0200

    chore: migrated riscv ports to new api

--- arch/riscv/rv32imac/mach-gd32vf103/intc-eclic.c
+++ arch/riscv/rv32imac/mach-gd32vf103/intc-eclic.c
@@ -13,28 +13,17 @@
 #define clicintattr(x) ((volatile uint8_t*)(ADDR_ECLIC + 0x1002 + 4 * (x)))
 #define clicintctl(x)  ((volatile uint8_t*)(ADDR_ECLIC + 0x1003 + 4 * (x)))
 
-/*@external@*/
-extern struct {
-    arch_isr_fn fn;
-    /*@temp@*/ /*@null@*/ void *priv;
-} ISR_TABLE[DEVICE_INTERRUPT_VECTOR_COUNT];
-
-void arch_register_interrupt(picoRTOS_irq_t irq, arch_isr_fn fn, void *priv)
+void arch_enable_interrupt_ext(picoRTOS_irq_t irq, picoRTOS_mask_t core_mask)
 {
-    arch_assert(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT, return );
-    ISR_TABLE[irq].fn = fn;
-    ISR_TABLE[irq].priv = priv;
-}
-
-void arch_enable_interrupt(picoRTOS_irq_t irq)
-{
+    /*@i@*/ (void)core_mask;
     arch_assert(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT, return );
     *clicintie(irq) = (uint8_t)1;
     *clicintattr(irq) = (uint8_t)1;
 }
 
-void arch_disable_interrupt(picoRTOS_irq_t irq)
+void arch_disable_interrupt_ext(picoRTOS_irq_t irq, picoRTOS_mask_t core_mask)
 {
+    /*@i@*/ (void)core_mask;
     arch_assert(irq < (picoRTOS_irq_t)DEVICE_INTERRUPT_VECTOR_COUNT, return );
     *clicintie(irq) = (uint8_t)0;
 }
